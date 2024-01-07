f66763373effc84b74edd0548f9cfb06660c9501
ngPsych <43138865+ngPsych@users.noreply.github.com>
2024-01-07 22:04:18 +0100

    fix

--- app/src/main/java/com/example/weshare/group/GroupActivity.kt
+++ app/src/main/java/com/example/weshare/group/GroupActivity.kt
@@ -1,16 +1,31 @@
 package com.example.weshare.group
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weshare.R
+import com.example.weshare.user.AuthManager
 
 class GroupActivity : AppCompatActivity() {
+
+    private val authManager = AuthManager()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group)
 
-        // Initialize and set up views, buttons, etc.
-        // Implement functionality to create groups, add/remove members, and view group details
+        val groupName = intent.getStringExtra("GROUP_NAME") ?: "Default Group Name"
+        val groupDescription = intent.getStringExtra("GROUP_DESCRIPTION") ?: "Default Description"
+
+        val groupNameTextView: TextView = findViewById(R.id.groupNameTextView)
+        val descriptionTextView: TextView = findViewById(R.id.descriptionTextView)
+
+        groupNameTextView.text = groupName
+        descriptionTextView.text = groupDescription
+
+        val nameTextView: TextView = findViewById(R.id.nameTextView)
+        nameTextView.text = authManager.getCurrentUserDetails()?.name
+
     }
 
     // Add methods to handle group interactions, UI updates, etc.
