891aaddd389d7759da11818c93ed40755ad3e963
ngPsych <43138865+ngPsych@users.noreply.github.com>
2024-01-07 18:53:07 +0100

    fix

--- app/src/main/java/com/example/weshare/group/Group.kt
+++ app/src/main/java/com/example/weshare/group/Group.kt
@@ -2,9 +2,9 @@ package com.example.weshare.group
 
 data class Group(
     val groupId: String? = null,
-    val name: String,
-    val description: String,
-    val creator: String,
-    val members: List<String> // List of user IDs
+    val name: String = "",
+    val description: String = "",
+    val creator: String = "",
+    val members: List<String> = listOf() // Initialize with an empty list
     // Add other relevant group details here
 )
