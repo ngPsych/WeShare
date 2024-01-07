fb7705211ade565701c96b49fb00746ee11002a2
ngPsych <43138865+ngPsych@users.noreply.github.com>
2024-01-07 18:49:10 +0100

    fix

--- app/src/main/java/com/example/weshare/user/User.kt
+++ app/src/main/java/com/example/weshare/user/User.kt
@@ -1,9 +1,9 @@
 package com.example.weshare.user
 
 data class User(
-    val userId: String? = null, // Make it nullable and default to null
-    val name: String,
-    val phoneNumber: String,
-    val email: String
-    // Add other relevant user details here
+    val userId: String = "", // Default values for all properties
+    val name: String = "",
+    val phoneNumber: String = "",
+    val email: String = ""
+    // Add other relevant user details with default values
 )
