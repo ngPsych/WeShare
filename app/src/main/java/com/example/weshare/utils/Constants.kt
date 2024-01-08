a5a5b6bd5f5ef70b2c00fbaedca174f838fd10d7
ngPsych <43138865+ngPsych@users.noreply.github.com>
2024-01-08 20:00:23 +0100

    notif

--- app/src/main/java/com/example/weshare/utils/Constants.kt
+++ app/src/main/java/com/example/weshare/utils/Constants.kt
@@ -3,8 +3,8 @@ package com.example.weshare.utils
 class Constants {
 
     companion object {
-        const val BASE_URL = "https:/fcm.googleapis.com"
-        const val SERVER_KEY = "BDTNvd9aRwtS5Olq8Cy9pGZqOWKfPYW-Mja-WmM__x-SI_QiOTF5TbrBY6k7Wrvp3Msq4g4H3eQF55H5TB36FO0"
+        const val BASE_URL = "https://fcm.googleapis.com/"
+        const val SERVER_KEY = "AAAAjQeM1Tc:APA91bF6CSPpGAxpRYcRxBOq1vONb_9gujbbTKvkhO6Fr5N2jjsMSzYyooXax8vqihp6XfwLNGrd88BkpQRpQvCLitVqBIjQxXbobc9kcOGnyKnu9qVHigdJ0db784eIENmfhnC7GCEF"
         const val CONTENT_TYPE = "application/json"
     }
 
